053ee2d51dd402210a6c97c51dfb552e7f3a46b0
Foxy <vovka71rus1820@gmail.com>
2026-07-03 16:06:11 +0300

    update server url

--- api/axios.ts
+++ api/axios.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const NewAxios = axios.create({
-  baseURL: "http://localhost:4200/",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+    baseURL: 'https://server-side-production-e7b3.up.railway.app/',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
